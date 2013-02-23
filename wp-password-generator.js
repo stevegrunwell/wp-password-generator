eb682325af910479953d3249e19ad86b5e5d039e
Steve Grunwell <stevegrunwell@gmail.com>
2013-02-23 17:28:32 -0500

    Change the localization variable from i18n to something more specific (wpPasswordGenerator); Finally removed the damn hyphen in the plugin name

--- wp-password-generator.js
+++ wp-password-generator.js
@@ -1,15 +1,17 @@
-/*
-  Plugin Name: WP-Password Generator
-  Plugin URI: http://stevegrunwell.com/wp-password-generator
-  Version: 2.4
-*/
-/*jslint white: true, maxerr: 50, indent: 4 */
+/**
+ * Scripting for the "Generate Password" button
+ * @package WordPress
+ * @subpackage WP Password Generator
+ * @author Steve Grunwell
+ */
+/** jslint white: true, maxerr: 50, indent: 4 */
 
-jQuery(function($){
+jQuery( function ( $ ) {
   "use strict";
-  $('#pass-strength-result').before('<br /><button id="password_generator" class="button-primary" value="' + i18n.generate + '" role="button">' + i18n.generate + '</button><br />');
 
-  $('#password_generator').on('click', function(){
+  $('#pass-strength-result').before( '<br /><button id="password_generator" class="button-primary" value="' + wpPasswordGenerator.generate + '" role="button">' + wpPasswordGenerator.generate + '</button><br />' );
+
+  $('#password_generator').on( 'click', function () {
     $.post(ajaxurl, { action : 'generate_password' }, function(p){
       $('#pass1, #pass2').val(p).trigger('keyup');
       $('#password_generator_toggle').find('kbd').html(p);
@@ -17,7 +19,7 @@ jQuery(function($){
       /** Append the 'Show password' link and bind the click event */
       if( $('#password_generator_toggle').length === 0 ){
         $('#send_password').prop('checked', 'checked'); // Only do this the first time
-        $('#password_generator').after('<span id="password_generator_toggle" style="margin-left:.25em;"><a href="#" role="button">' + i18n.show + '</a></span>');
+        $('#password_generator').after('<span id="password_generator_toggle" style="margin-left:.25em;"><a href="#" role="button">' + wpPasswordGenerator.show + '</a></span>');
         $('#password_generator_toggle').on('click', 'a', function(){
           $(this).fadeOut(200, function(){
             $('#password_generator_toggle').html('<kbd style="font-size:1.2em;">' + $('#pass1').val() + '</kbd>');
--- wp-password-generator.php
+++ wp-password-generator.php
@@ -1,13 +1,17 @@
 <?php
-/*
-  Plugin Name: WP-Password Generator
-  Plugin URI: http://stevegrunwell.com/wp-password-generator
-  Description: Generates a random password when creating a new WP user
-  Version: 2.5
-  Author: Steve Grunwell
-  Author URI: http://stevegrunwell.com
-  License: GPL2
-*/
+/**
+ * Plugin Name: WP Password Generator
+ * Plugin URI: http://stevegrunwell.com/wp-password-generator
+ * Description: Generates a random password when creating a new WP user
+ * Version: 2.5
+ * Author: Steve Grunwell
+ * Author URI: http://stevegrunwell.com
+ * License: GPL2
+ *
+ * @package WordPress
+ * @subpackage WP Password Generator
+ * @author Steve Grunwell
+ */
 
 define( 'WP_PASSWORD_GENERATOR_VERSION', '2.5' );
 
@@ -21,8 +25,6 @@ define( 'WP_PASSWORD_GENERATOR_VERSION', '2.5' );
  * @return void
  * @uses get_option()
  * @uses update_option()
- * @package WordPress
- * @subpackage WP Password Generator
  * @since 2.1
  */
 function wp_password_generator_install() {
@@ -62,38 +64,34 @@ function wp_password_generator_install() {
  * @uses plugins_url()
  * @uses wp_enqueue_script()
  * @uses wp_localize_script()
- * @package WordPress
- * @subpackage WP Password Generator
  * @since 1.0
  */
 function wp_password_generator_load() {
   $current_screen = get_current_screen();
   if ( in_array( $current_screen->base, array( 'profile', 'user', 'user-edit' ) ) ) {
     wp_enqueue_script( 'wp-password-generator', plugins_url( 'wp-password-generator.js', __FILE__ ), array( 'jquery' ), WP_PASSWORD_GENERATOR_VERSION, true );
-    wp_localize_script( 'wp-password-generator', 'i18n', array(
-      'generate'  => __( 'Generate Password', 'wp-password-generator' ),
-      'show'      => __( 'Show Password', 'wp-password-generator' )
-    ));
+    $i18n = array(
+      'generate' => __( 'Generate Password', 'wp-password-generator' ),
+      'show' => __( 'Show Password', 'wp-password-generator' )
+    );
+    wp_localize_script( 'wp-password-generator', 'wpPasswordGenerator', $i18n );
   }
   return;
 }
 
 /**
  * Handle an Ajax request for a password, print response.
- *
  * Uses wp_generate_password(), a pluggable function within the WordPress core
  *
  * @return void (echoes password)
  * @uses get_option()
  * @uses wp_generate_password()
  * @uses wp_password_generator_install()
- * @package WordPress
- * @subpackage WP Password Generator
  * @since 1.0
  */
 function wp_password_generator_generate() {
   $opts = get_option( 'wp-password-generator-opts', false );
-  if( !$opts || $opts['version'] < WP_PASSWORD_GENERATOR_VERSION ){ // No options or an older version
+  if ( ! $opts || $opts['version'] < WP_PASSWORD_GENERATOR_VERSION ) { // No options or an older version
     wp_password_generator_install();
     $opts = get_option( 'wp-password-generator-opts', false );
   }
@@ -106,5 +104,3 @@ function wp_password_generator_generate() {
 add_action( 'admin_print_scripts', 'wp_password_generator_load' ); // run wp_password_generator_load() during admin_print_scripts
 add_action( 'wp_ajax_generate_password', 'wp_password_generator_generate' ); // Ajax hook
 register_activation_hook( __FILE__, 'wp_password_generator_install' );
-
-?>
