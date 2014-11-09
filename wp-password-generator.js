d037a8942715892bc4d8f82201e20b6773bc6387
Steve Grunwell <stevegrunwell@gmail.com>
2014-11-09 11:32:02 -0500

    Adjust spacing

--- wp-password-generator.js
+++ wp-password-generator.js
@@ -7,23 +7,25 @@
  */
 /** jslint white: true, maxerr: 50, indent: 4 */
 
-jQuery( function ( $ ) {
+jQuery(function ($) {
   "use strict";
 
-  $('#pass-strength-result').before( '<br /><button id="password_generator" class="button-primary" value="' + wpPasswordGenerator.generate + '" role="button">' + wpPasswordGenerator.generate + '</button><br />' );
+  $('#pass-strength-result').before('<br /><button id="password_generator" class="button-primary" value="' + wpPasswordGenerator.generate + '" role="button">' + wpPasswordGenerator.generate + '</button><br />');
 
-  $('#password_generator').on( 'click', function () {
-    $.post( ajaxurl, { action: 'generate_password' }, function( p ) {
-      $('#pass1, #pass2').val( p ).trigger( 'keyup' );
-      $('#password_generator_toggle').find( 'kbd' ).html( p );
+  $('#password_generator').on('click', function () {
+    $.post(ajaxurl, {
+      action: 'generate_password'
+    }, function(p) {
+      $('#pass1, #pass2').val(p).trigger('keyup');
+      $('#password_generator_toggle').find('kbd').html(p);
 
       /** Append the 'Show password' link and bind the click event */
-      if ( $('#password_generator_toggle').length === 0 ) {
-        $('#send_password').prop( 'checked', 'checked' ); // Only do this the first time
-        $('#password_generator').after( '<span id="password_generator_toggle" style="margin-left:.75em;"><a href="#" role="button">' + wpPasswordGenerator.show + '</a></span>' );
-        $('#password_generator_toggle').on( 'click', 'a', function() {
-          $( this ).fadeOut( 200, function() {
-            $('#password_generator_toggle').html( '<kbd style="font-size:1.2em;">' + $('#pass1').val() + '</kbd>' );
+      if ( $('#password_generator_toggle').length === 0) {
+        $('#send_password').prop('checked', true); // Only do this the first time
+        $('#password_generator').after('<span id="password_generator_toggle" style="margin-left:.75em;"><a href="#" role="button">' + wpPasswordGenerator.show + '</a></span>');
+        $('#password_generator_toggle').on('click', 'a', function() {
+          $(this).fadeOut(200, function () {
+            $('#password_generator_toggle').html('<kbd style="font-size:1.2em;">' + $('#pass1').val() + '</kbd>');
           });
           return false;
         });
